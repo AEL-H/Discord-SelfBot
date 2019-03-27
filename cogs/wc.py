2d142a2aa88875e7b18c6d775735bf22f59ff422
Amir <amir@amir.localdomain>
2019-03-27 13:22:41 +0000

    removed superfluous function

--- cogs/wc.py
+++ cogs/wc.py
@@ -39,23 +39,6 @@ class WC:
                 continue
             text += message.content
             text += " "
-        wc = WordCloud(width=RESOLUTION['width'], height=RESOLUTION['height'], background_color=BG_COLOUR, max_words=MAX_WORDS, mode=MODE).generate(text)
-        wc.to_file(FILENAME)
-        wordcloudfile = discord.File(fp=open(os.path.join(os.getcwd(),FILENAME),'rb'))
-        await channel.send(file=wordcloudfile)
-        
-    @commands.command(pass_context=True)
-    async def wcc(self, ctx):
-        print("Working")
-        user = ctx.message.mentions[0]
-        print(user.id)
-        text = ""
-        channel = ctx.message.channel
-        async for message in channel.history(limit=None):
-            if message.author.id != user.id:
-                continue
-            text += message.content
-            text += " "
         the_mask = np.array(Image.open(MASK_PATH))
         wc = WordCloud(width=RESOLUTION['width'], height=RESOLUTION['height'], background_color=BG_COLOUR, max_words=MAX_WORDS,font_path=FONT_PATH, mask=the_mask).generate(text)        
         wc.to_file(FILENAME)
