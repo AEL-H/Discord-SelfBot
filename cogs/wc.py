9f9d0b4d1e74d3e87afa9440883c0995cfa8c2e5
Amir <amir@amir.localdomain>
2019-03-27 12:45:11 +0000

    added wordcloud cog wc.py

---
+++ cogs/wc.py
@@ -0,0 +1,81 @@
+from discord.ext import commands
+from cogs.utils.checks import *
+from os import path
+from PIL import Image
+import io
+import os
+import sys
+from wordcloud import WordCloud
+from wordcloud import STOPWORDS
+import io
+from PIL import Image
+import discord
+import matplotlib
+import numpy as np
+
+matplotlib.use('Agg')
+
+class WC:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def wc(self, ctx):
+        print("Working")
+        user = ctx.message.mentions[0]
+        print(user.id)
+        text = ""
+        #server = ctx.message.guild
+        #print(server.id)
+        channel = ctx.message.channel
+        async for message in channel.history(limit=10**10):
+            if message.author.id != user.id:
+                continue
+            text += message.content
+            text += " "
+        #for channel in server.channels:
+        #    try:
+        #        async for msg in self.bot.logs_from(channel, limit=10**10):
+        #            if msg.author.id != user.id:
+        #                continue
+        #            text += msg.content
+        #            text += " "
+        #    except Exception:
+        #        continue
+        wc = WordCloud(width=1920, height=1080, background_color="white", max_words=1000).generate(text)
+        wc.to_file("wordcloud.png")
+        wordcloudfile = discord.File(fp=open(os.path.join(os.getcwd(),"wordcloud.png"),'rb'))
+        await channel.send(file=wordcloudfile)
+        
+    @commands.command(pass_context=True)
+    async def wcc(self, ctx):
+        print("Working")
+        user = ctx.message.mentions[0]
+        print(user.id)
+        text = ""
+        #server = ctx.message.guild
+        #print(server.id)
+        channel = ctx.message.channel
+        async for message in channel.history(limit=10**10):
+            if message.author.id != user.id:
+                continue
+            text += message.content
+            text += " "
+        #for channel in server.channels:
+        #    try:
+        #        async for msg in self.bot.logs_from(channel, limit=10**10):
+        #            if msg.author.id != user.id:
+        #                continue
+        #            text += msg.content
+        #            text += " "
+        #    except Exception:
+        #        continue
+        the_mask = np.array(Image.open("heartmask.png"))
+        wc = WordCloud(width=1920, height=1080, background_color="white", max_words=1000,font_path="C:/Windows/Fonts/I Love What You Do!!...ttf", mask=the_mask).generate(text)        
+        wc.to_file("wordcloud.png")
+        wordcloudfile = discord.File(fp=open(os.path.join(os.getcwd(),"wordcloud.png"),'rb'))
+        await channel.send(file=wordcloudfile)        
+
+
+def setup(bot):
+    bot.add_cog(WC(bot))
